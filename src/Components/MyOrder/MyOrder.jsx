54999685abe49b71440c0d08e5467bde196bce01
mrazadtamal <mrazadt@gmail.com>
2022-01-06 22:57:24 +0600

    done

--- src/Components/MyOrder/MyOrder.jsx
+++ src/Components/MyOrder/MyOrder.jsx
@@ -39,21 +39,56 @@ const MyOrder = () => {
       <h1 className="mt-16 text-4xl text-center ">My Order</h1>
 
       {data.map((item) => (
-        <div
-          className="border-2 m-auto border-gray-600 mt-16 w-60 h-60 text-center rounded-lg "
-          key={item._id}
-        >
-          <h1 key={item._id}>Name : {item.name}</h1>
-          <h1>Email : {item.email}</h1>
-          <h1>Place : {item.Place}</h1>
-          <h1>Phone : {item.phone}</h1>
-          <button
-            className="py-4 px-6  mt-7 text-lg font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-            onClick={() => handleDelete(item._id)}
-          >
-            Cancel
-          </button>
-        </div>
+        <section className="bg-white dark:bg-gray-900">
+          <div className="container px-6 py-10 mx-auto">
+            <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+              <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+                <span className="inline-block text-blue-500 dark:text-blue-400">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                    />
+                  </svg>
+                </span>
+
+                <h1
+                  key={item._id}
+                  className="text-2xl font-semibold text-gray-700 capitalize dark:text-white"
+                >
+                  Name : {item.name}
+                </h1>
+
+                <p className="text-gray-500 dark:text-gray-300">
+                  Email : {item.email}
+                </p>
+
+                <p className="text-gray-500 dark:text-gray-300">
+                  Place : {item.Place}
+                </p>
+
+                <p className="text-gray-500 dark:text-gray-300">
+                  Phone : {item.phone}
+                </p>
+
+                <button
+                  className="py-3 px-6  mt-7 text-lg font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+                  onClick={() => handleDelete(item._id)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       ))}
     </div>
   );
