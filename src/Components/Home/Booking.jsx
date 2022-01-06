8b5c00614176a05df325841fd7ea920e4be4f60a
mrazadtamal <mrazadt@gmail.com>
2022-01-06 22:13:06 +0600

    card

--- src/Components/Home/Booking.jsx
+++ src/Components/Home/Booking.jsx
@@ -24,13 +24,44 @@ const Booking = () => {
   if (data === {}) return "Loading...";
 
   return (
-    <div>
-      <img className="w-80 h-60 mt-16 ml-16" src={data.img} alt="img" />
-      <h1 className="ml-16 mt-3">Country : {data.Country} </h1>
-      <h1 className="ml-16 mt-3">Price: {data.Price} </h1>
-      <h1 className="ml-16 mt-3">Rating: {data.Rating} </h1>
-      <h1 className="ml-16 mt-3">name: {data.name} </h1>
-      <h1 className="ml-16 mt-3 w-80">Description : {data.des} </h1>
+    <div className=" flex">
+      <div className="max-w-2xl mx-auto">
+        <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+          <a href="/">
+            <img className="rounded-t-lg mt-11" src={data.img} alt="" />
+          </a>
+          <div className="p-5">
+            <a href="/">
+              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <a
+              href="/"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                className="-mr-1 ml-2 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <Order />
     </div>
   );
