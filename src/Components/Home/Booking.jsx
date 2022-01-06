cabcd015ae3cea8fa6bde3d8e189e8fe93edfea8
mrazadtamal <mrazadt@gmail.com>
2022-01-06 22:19:31 +0600

    card

--- src/Components/Home/Booking.jsx
+++ src/Components/Home/Booking.jsx
@@ -25,44 +25,37 @@ const Booking = () => {
 
   return (
     <div className=" flex">
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-2xl mx-auto mt-11">
         <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
           <a href="/">
-            <img className="rounded-t-lg mt-11" src={data.img} alt="" />
+            <img className="rounded-t-lg" src={data.img} alt="" />
           </a>
           <div className="p-5">
-            <a href="/">
-              <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
+            <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+              Country : {data.Country}
+            </h5>
+
             <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+              Price: {data.Price}
             </p>
-            <a
-              href="/"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                className="-mr-1 ml-2 h-4 w-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
+
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              Rating: {data.Rating}
+            </p>
+
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              name: {data.name}
+            </p>
+
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
+              Description : {data.des}
+            </p>
           </div>
         </div>
       </div>
-
-      <Order />
+      <div className=" mr-28">
+        <Order />
+      </div>
     </div>
   );
 };
